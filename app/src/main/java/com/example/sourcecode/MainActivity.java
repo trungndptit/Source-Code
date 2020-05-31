b1a23e8ced670523540752389e965b9675d50dcf
trungndptit <trungndptit@gmail.com>
2020-05-31 23:57:12 +0700

    code permission: over lay, setting, storage

--- app/src/main/java/com/example/sourcecode/MainActivity.java
+++ app/src/main/java/com/example/sourcecode/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.sourcecode;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_OVERLAY = 1001;
+    private static final int PERMISSIONS_REQUEST_STORAGE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_OVERLAY:
+                if (resultCode == RESULT_CANCELED){
+                    // do something here, could check permission and require again.
+                }
+                break;
+        }
+    }
+
+    private void requireOverLayPermission(){
+        if (!Settings.canDrawOverlays(MainActivity.this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, PERMISSIONS_REQUEST_OVERLAY);
+        }
+    }
+
+    private void requireStoragePermission(){
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            MainActivity.this.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_STORAGE);
+        }
+    }
+
+    private boolean checkSystemWritePermission() {
+        boolean retVal = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            retVal = Settings.System.canWrite(this);
+            if(retVal){
+                Toast.makeText(this, "Write allowed :-)", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Write not allowed :-(", Toast.LENGTH_LONG).show();
+            }
+        }
+        return retVal;
+    }
+
+    // If !checkSystemWritePermission() do openAndroidPermissionsMenu()
+    private void openAndroidPermissionsMenu() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
     }
 }
