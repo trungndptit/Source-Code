8b6f1738e101e49efbf9846362e2fb1efcc7905c
trungndptit <trungndptit@gmail.com>
2020-06-04 16:46:53 +0700

    alert dialog full screen

--- app/src/main/java/com/example/sourcecode/MainActivity.java
+++ app/src/main/java/com/example/sourcecode/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.sourcecode;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_hello);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+        // width và height ngoài cùng của custom layout phải để là wrapcontent.
+        // Sau view đấy thì mở là match parent hoặc tùy ý.
+
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customDialogView = layoutInflater.inflate(R.layout.custom_alert_dialog, null);
+        TextView mTvOk = customDialogView.findViewById(R.id.tv_ok);
+
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(MainActivity.this,
+                        android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+        builder.setView(customDialogView);
+
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        mTvOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
     }
 }
